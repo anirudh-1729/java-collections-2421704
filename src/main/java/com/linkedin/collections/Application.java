301ae7abee2e0ac6bd912533a1e4092d268a9b0e
anirudh-1729 <anirudhmohanty.15april@gmail.com>
2024-07-05 20:42:11 +0530

    .

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,7 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Application {
 
@@ -12,7 +13,38 @@ public class Application {
 		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 		
-		Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
+		Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge,piccadilly, westminister));
+
+		Iterator<Room> iter = rooms.iterator();
+		Iterator<Room> iter2 = rooms.iterator();
+		
+		// System.out.println(iter.next().getName());
+		// System.out.println(iter.next().getName());
+		// System.out.println(iter.next().getName());
+
+		// first loop
+		System.out.println("1");
+		while (iter.hasNext()) {
+			
+			Room rooms2 = iter.next();
+			System.out.println(rooms2.getName() + " " + rooms2.getRate() + " " +  rooms2.getType());
+		}
+
+		//second loop
+		System.out.println("2");
+		while (iter2.hasNext()) {
+
+			System.out.println( iter2.next().getName() + " " + iter2.next().getRate() + iter2.next().getType());
+			
+		}
+
+		// third loop
+		System.out.println("3");
+		for (Room room : rooms) {
+			
+			System.out.println(room.getName() + " " + room.getRate() + " " + room.getType());
+		}
+
 		
 			
 	}
