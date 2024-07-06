364fae90583cf0632032ebe73f3c6f5350752757
anirudh-1729 <anirudhmohanty.15april@gmail.com>
2024-07-06 22:49:16 +0530

    v

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,6 @@
 package com.linkedin.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -10,6 +11,22 @@ public class Application {
 		Guest maria = new Guest("Maria", "Doe", false);
 		Guest sonia = new Guest("Sonia", "Doe", true);
 		Guest siri = new Guest("Siri", "Doe", true);
+
+		List<Guest> members = new ArrayList<>();
+
+		members.add(john);
+		members.add(maria);
+		
+
+		print(members);
+
+		members.add(0, siri);
+
+		print(members);
+
+		members.get(1).setLoyaltyProgramMember(true);
+
+		print(members);
 
 
 	}
